7fea700c9801347ad979a3ffd856a55c02d01e41
Dhanush Ekollu <66331423+DEboy2007@users.noreply.github.com>
2021-12-05 18:35:50 +0000

    Added a few basic commands

---
+++ main.py
@@ -0,0 +1,37 @@
+import discord
+from discord.ext import commands, tasks
+
+with open("token.txt") as token:
+    TOKEN = token.read()
+
+help_message = """Use prefix $
+*$info* - Displays this message
+*$hello* - Say hello"""
+
+client = commands.Bot(command_prefix="$")
+
+
+@client.event
+async def on_ready():
+    print("Bot Logged In")
+
+
+@client.command()
+async def info(ctx):
+    await ctx.send(help_message)
+
+
+@client.command()
+async def hello(ctx):
+    await ctx.send("Hello!")
+
+@client.command()
+async def amogus(ctx):
+    await ctx.send("sus")
+
+@client.command()
+async def wiki(ctx, arg):
+  await ctx.send(f"https://en.wikipedia.org/wiki/{arg}")
+
+
+client.run(TOKEN)
