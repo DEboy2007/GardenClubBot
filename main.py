b18947b6fcd13dd3db6775c50d819ab40448cabb
Dhanush Ekollu <66331423+DEboy2007@users.noreply.github.com>
2021-12-12 18:05:45 +0000

    Nothing changed here ;)

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import discord
 from discord.ext import commands, tasks
 import keep_alive
+import random
+
+responses = ["You missed dum dum", "You hit lol", "You're a coward and dropped the snow because it was too cold"]
 
 with open("token.txt") as token:
     TOKEN = token.read()
@@ -65,7 +68,14 @@ async def yt(ctx, arg):
     new_str = new_str[:-1]
     await ctx.send(f"https://www.youtube.com/results?search_query={new_str}")
 
+@client.command()
+async def throw(ctx, arg):
+  await ctx.send(f"{random.choice(responses)}. Snowball throwed towards {arg}")
 
+@client.command()
+async def youaregay(ctx):
+    await ctx.send("NO YOU YOURE THE ONE WHOSE MOM IS UNDER LEGAL DRINKING AGE GET A LIFE GET A GIRLFREIND!")
+    
 keep_alive.keep_alive()
 
 client.run(TOKEN)
